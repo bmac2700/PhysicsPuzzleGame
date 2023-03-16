402fc1bd9b1e9f5e02fae8643c27c3c4243d6186
bmac2700 <bmac2700@proton.me>
2023-03-16 13:56:27 +0200

    Fixed type in the window title

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "Bowen's handyman service".into(),
+                title: "Bowens' handyman service".into(),
                 // Tells wasm to resize the window according to the available canvas
                 fit_canvas_to_parent: true,
                 // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
